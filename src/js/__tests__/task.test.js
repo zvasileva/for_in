175aca309133f0135957b9f69bbea96980bbc586
zvasileva <zojka-homka@rambler.ru>
2020-06-09 00:35:22 +0300

    Готово

--- src/js/__tests__/task.test.js
+++ src/js/__tests__/task.test.js
@@ -30,12 +30,12 @@ test.each([
 		{
 			name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
 		},
-		['name', 'level', 'defence'],
+		['name', 'level'],
 		[
 			{ key: 'name', value: 'мечник' },
 			{ key: 'level', value: 2 },
-			{ key: 'defence', value: 40 },
 			{ key: 'attack', value: 80 },
+			{ key: 'defence', value: 40 },
 			{ key: 'health', value: 10 },
 		],
 	],
